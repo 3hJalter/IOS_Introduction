827846ee3b66da1a73e0866b1a916e01c41891a3
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2023-10-17 14:03:19 +0700

    Add DES solution

---
+++ EngSlide/EncryptDES/EncryptDES/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using EncryptDES.Lib;
+
+const string MAIN_KEY = "AABB09182736CCDD";
+const string PLAIN_TEXT = "Ha Huy Hoang";
+
+RoundKey roundKey = new(Utilities.HexToBinary(MAIN_KEY));
+roundKey.Print();
+
+Console.WriteLine($"Plain text: {PLAIN_TEXT}.");
+string cipherText = Des.Encrypt(PLAIN_TEXT, roundKey);
+Console.WriteLine($"Cipher text: {cipherText}");
+Console.WriteLine($"Decrypted text: {Des.Decrypt(cipherText, roundKey)}.");
+
+
