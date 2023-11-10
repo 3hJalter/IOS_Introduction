e13f7254a28ee897bb786f4c605805c33e5c7f28
Hà Huy Hoàng <101927338+3hJalter@users.noreply.github.com>
2023-11-10 18:43:51 +0700

    Add Exercise 5 solution

---
+++ ExerciseSolution/C5_PKC_Knapsack/Lib/MerkleHelmanCrypto.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace C5_PKC_Knapsack.Lib;
+
+public class MerkleHelmanCrypto
+{
+    private readonly List<int> aSuperIncreasing;
+    private readonly long m;
+    private readonly long miOmega;
+    private readonly List<long> a;
+    public MerkleHelmanCrypto(string plainText)
+    {
+        aSuperIncreasing = new List<int> {1};
+        for (int i = 1; i < plainText.Length; i++)
+        {
+            int element = aSuperIncreasing.Sum() + 1;
+            aSuperIncreasing.Add(element);
+        }
+        Console.WriteLine(string.Join(", ", aSuperIncreasing));
+        m = aSuperIncreasing.Sum() + 10; // Use long for larger numbers
+        long omega = Utilities.GetRandomCoPrime(m); // co-prime with m
+        miOmega = Utilities.InverseModulo(omega, m); // Modular multiplicative inverse of omega (mod m)
+        a = new List<long>(aSuperIncreasing.Select(i => i * omega % m));
+        Console.WriteLine($"Super-increasing vector: {string.Join(", ", aSuperIncreasing)}");
+        Console.WriteLine($"m = {m}");
+        Console.WriteLine($"Omega: {omega}");
+        Console.WriteLine($"Multiplicative inverse of Omega (mod m): {miOmega}");
+        Console.WriteLine("Public key: " + string.Join(", ", a));
+    }
+
+    public long Encryption(string plainText)
+    {
+        long cipherText = plainText.Select((t, i) => a[i] * int.Parse(t.ToString())).Sum();
+        Console.WriteLine($"T = {cipherText}");
+        return cipherText;
+    }
+
+    public void Decryption(long cipherText)
+    {
+        StringBuilder newPlainText = new();
+        long newT = cipherText * miOmega % m;
+        if (newT < 0) newT += m; // Ensure the result is non-negative
+        Console.WriteLine($"T' = {cipherText} * {miOmega} % {m} = {newT}");
+
+        for (int i = aSuperIncreasing.Count - 1; i >= 0; i--)
+        {
+            char c;
+            if (newT >= aSuperIncreasing[i])
+            {
+                c = '1';
+                newT -= aSuperIncreasing[i];
+                Console.WriteLine($"T'[{i:00}]: {newT,-6} = T[{i + 1:00}] - {aSuperIncreasing[i],-6} => X[{i:00}]: {c}");
+            }
+            else
+            {
+                c = '0';
+                Console.WriteLine($"T'[{i:00}]: {newT,-6} = T[{i + 1:00}] => X[{i:00}]: {c}");
+            }
+
+            newPlainText.Insert(0, c);
+        }
+
+        Console.WriteLine($"Result of the decryption: {newPlainText}");
+        Console.WriteLine($"Original name: {Utilities.DecodeBinaryToText(newPlainText.ToString())}");
+    }
+}
---
+++ ExerciseSolution/C5_PKC_Knapsack/Lib/Utilities.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+
+namespace C5_PKC_Knapsack.Lib;
+
+public static class Utilities
+{
+    public static string? EncodeToBinary(string text)
+    {
+        StringBuilder binaryStringBuilder = new StringBuilder();
+
+        foreach (char c in text)
+        {
+            int charValue;
+            switch (c)
+            {
+                // check if c is a uppercase
+                case >= 'A' and <= 'Z':
+                    charValue = c - 'A'; // Assuming uppercase letters
+                    break;
+                case >= 'a' and <= 'z':
+                    charValue = c - 'a'; // Assuming lowercase letters
+                    break;
+                default:
+                    return null; // c is not a letter
+            }
+            string binaryRepresentation = Convert.ToString(charValue, 2).PadLeft(5, '0');
+            binaryStringBuilder.Append(binaryRepresentation);
+        }
+
+        return binaryStringBuilder.ToString();
+    }
+
+    public static string DecodeBinaryToText(string binaryText)
+    {
+        StringBuilder textStringBuilder = new();
+
+        for (int i = 0; i < binaryText.Length; i += 5)
+        {
+            string binaryRepresentation = binaryText.Substring(i, 5);
+            int charValue = Convert.ToInt32(binaryRepresentation, 2);
+            char c = (char)(charValue + 'A');
+            textStringBuilder.Append(c);
+        }
+
+        return textStringBuilder.ToString();
+    }
+
+    public static long InverseModulo(long number, long modulo)
+    {
+        for (long i = 1; i < modulo; i++)
+        {
+            if ((number * i) % modulo == 1)
+            {
+                return i;
+            }
+        }
+        throw new ArithmeticException("Modular inverse does not exist.");
+    }
+
+    public static long GetRandomCoPrime(long number)
+    {
+        Random random = new();
+        long randomCoPrime = random.Next(2, (int)number);
+        while (Gcd(number, randomCoPrime) != 1)
+        {
+            randomCoPrime = random.Next(2, (int)number);
+        }
+        return randomCoPrime;
+    }
+
+    private static long Gcd(long number, long randomCoPrime)
+    {
+        while (randomCoPrime != 0)
+        {
+            long temp = randomCoPrime;
+            randomCoPrime = number % randomCoPrime;
+            number = temp;
+        }
+        return number;
+    }
+}
